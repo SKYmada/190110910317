2702f37d329d0d11445ee15b5cb135c304137201
mjt <2779734113@qq.com>
2021-12-29 15:56:49 +0800

    欢迎界面以及登陆页面

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 const express = require("express"); //导入express框架
-
+const ejs = require("ejs")
 const app = express();
 
-app.use('/',express.static("public"));
-app.use('/',express.static("photo"));
-app.get('/');
+app.use('/photo',express.static("photo"));
+app.use(express.static("node_modules"));
+
+//欢迎页面
+app.get('/',(req, res)=>{
+    res.render('index.ejs', {
+        username:null
+    })
+})
+app.get('/index.ejs',(req, res)=>{
+
+    res.render('index.ejs', {
+        username:null
+    })
+
+})
+
+//登陆页面
+app.get('/login.ejs',(req, res)=>{
+
+    res.render('login.ejs', {
+        username:null
+    })
+
+})
+
 
 app.listen(10317, console.log("服务器就绪"))
