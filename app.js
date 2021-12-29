5f308ba24f43ae45e96c23af1a1d8b0e5f300623
mjt <2779734113@qq.com>
2021-12-29 14:50:36 +0800

    install bootstrap4.4.1

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ const express = require("express"); //导入express框架
 
 const app = express();
 
-app.get('/', (req,res) => {
-    res.send("服务器启动成功")
-
-})
+app.use('/',express.static("public"));
+app.use('/',express.static("photo"));
+app.get('/');
 
 app.listen(10317, console.log("服务器就绪"))
